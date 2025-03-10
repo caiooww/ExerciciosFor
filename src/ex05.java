545dbd3a3799d60bb050492b73eda1f06c4d00fc
caiooww <caiomksp@gmail.com>
2025-03-10 18:55:56 -0300

    Envio dos exercícios 05, 06, e 07

---
+++ src/ex05.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ex05 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor de n:");
+        int n = sc.nextInt();
+
+        int fat = 1;
+        for (int i=1; i<=n; i++) {
+            fat = fat * i;
+        }
+
+        System.out.println(fat);
+
+        sc.close();
+    }
+}
+
